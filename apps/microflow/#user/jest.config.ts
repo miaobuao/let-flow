1e45a7648583b5c6169800434dbb99b43ce0527b
yangqiuyi <yangqiuyi@yaocheng.cn>
2023-11-09 13:48:59 +0800

    update structure

--- apps/microflow/#user/jest.config.ts
+++ apps/microflow/#user/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'apps/microflow/ms-userflow',
+  displayName: 'ms/@user',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/apps/microflow/ms-userflow',
+  coverageDirectory: '../../../coverage/apps/ms/@user',
 };
